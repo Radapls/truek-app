be0664ef1882f1c936f25d9ab6149fb2acc1e916
Juan Felipe Rada <j.f.r.a1997@gmail.com>
2023-01-04 22:54:19 -0300

    style: Add styles to principal component in web application

--- web/src/App.js
+++ web/src/App.js
@@ -7,23 +7,88 @@
 // State
 // Información que el componente va a manipular. Informaciones mantenidas por el componente //TODO(Concepto: Inmutabilidad)
 
-import { useState } from "react";
-import Header from "./Header";
+
+import './App.css';
+import './global.css';
+import './Main.css';
+import './Sidebar.css';
 
 function App() {
-
-  let [counter, setCounter] = useState(0)
-
-  function incrementCounter(){
-    setCounter( counter + 1)
-  }
   return (
-    <>
-    <h1> Contador: {counter}</h1>
-    <button onClick={incrementCounter}>Incrementar</button>
+    <div id="app">
+      <aside>
+      <strong>
+      <form>
 
-    <Header title="das"/>
-    </>
+        <div class="input-block">
+          <label htmlFor=''>Usuario de Github</label>
+          <input name='github_username' id='username_github'/>
+        </div>
+
+        <div class="input-block">
+          <label htmlFor=''>Tecnologias</label>
+          <input name='techs' id='techs'/>
+        </div>
+
+        <div class="input-group">
+          <div class="input-block">
+            <label htmlFor=''>Latitud</label>
+            <input name='latitude' id='latitude'/>
+          </div>
+
+          <div class="input-block">
+            <label htmlFor=''>Longitud</label>
+            <input name='longitude' id='longitude'/>
+          </div>
+
+        </div>
+
+        <button type='submit'>Enviar</button>
+
+      </form>
+      </strong>
+      </aside>
+
+      <main>
+        <ul>
+          <li className='dev-item'>
+            <header>
+              <img src="https://avatars.githubusercontent.com/u/63562282?v=4"  alt="_blank"/>
+              <div class="user-info">
+                <strong> Juan Felipe Rada</strong>
+                <span>Angular, TypeScript</span>
+              </div>
+            </header>
+            <p>Politólogo* Aprendiendo a programar una linea a la vez</p>
+            <a href="https://github.com/Radapls">Acceder al perfil de github</a>
+          </li>
+
+          <li className='dev-item'>
+            <header>
+              <img src="https://avatars.githubusercontent.com/u/63562282?v=4" alt="_blank"/>
+              <div class="user-info">
+                <strong> Juan Felipe Rada</strong>
+                <span>Angular, TypeScript</span>
+              </div>
+            </header>
+            <p>Politólogo* Aprendiendo a programar una linea a la vez</p>
+            <a href="https://github.com/Radapls">Acceder al perfil de github</a>
+          </li>
+
+          <li className='dev-item'>
+            <header>
+              <img src="https://avatars.githubusercontent.com/u/63562282?v=4" alt="_blank"/>
+              <div class="user-info">
+                <strong> Juan Felipe Rada</strong>
+                <span>Angular, TypeScript</span>
+              </div>
+            </header>
+            <p>Politólogo* Aprendiendo a programar una linea a la vez</p>
+            <a href="https://github.com/Radapls">Acceder al perfil de github</a>
+          </li>
+        </ul>
+      </main>
+    </div>
   );
 }
 
