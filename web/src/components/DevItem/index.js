9d43c5b6c64007cb852777d5d7a5347f2cb7b429
Juan Felipe Rada <j.f.r.a1997@gmail.com>
2023-01-06 00:15:49 -0300

    feat: Update

--- web/src/components/DevItem/index.js
+++ web/src/components/DevItem/index.js
@@ -18,8 +18,10 @@ function DevItem({dev}){
     return(
         <li className='dev-item'>
         <header>
+
             <img src={dev.avatar_url}  alt={dev.name}/>
             <div className="user-info">
+
                 <strong> {dev.name}</strong>
                 <span>{dev.techs.join(', ')}</span>
             </div>
